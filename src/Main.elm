530c7ed8cc695dd612c3793efdffc9c2640b1309
Dan Bruder <ddbruder@gmail.com>
2019-04-07 07:22:41 -0400

    started work on header

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), header, init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import FeatherIcons
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
 
 
 ---- MODEL ----
@@ -37,8 +39,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ header model
         ]
 
 
@@ -54,3 +55,28 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+
+---- Other stuff ----
+
+
+header : Model -> Html Msg
+header model =
+    div [ class "flex bb b--light-gray w-100 items-center" ]
+        [ div [ class "w5 flex pv3 ph4" ]
+            [ FeatherIcons.menu
+                |> FeatherIcons.toHtml []
+            , h1 [ class "f4 mid-gray ma0 mb1 ml3" ] [ text "Calendar" ]
+            ]
+        , div [ class "w-100 flex bl b--light-gray pv3 ph4 items-center" ]
+            [ button [ class "pointer f6 bg-white ba b--light-silver br2 ph3 pv2" ] [ text "Today" ]
+            , div [ class "flex pl3" ]
+                [ FeatherIcons.chevronLeft
+                    |> FeatherIcons.toHtml []
+                , FeatherIcons.chevronRight
+                    |> FeatherIcons.toHtml []
+                ]
+            , h2 [ class "pl4 fw1 ma0 pa0 mb1 f3" ] [ text "April 2019" ]
+            ]
+        ]
